caab263f3f6753aa3646a1933a6cb25a9e442dd2
divisrihayanti <115920560+divisrihayanti@users.noreply.github.com>
2022-10-30 20:17:30 +0700

    Add files via upload

---
+++ dstore2/app/src/main/java/com/example/dstore/store4.java
@@ -0,0 +1,14 @@
+package com.example.dstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class store4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store5);
+    }
+}
